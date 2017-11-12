a4752340f327386747829b02af2ee8d9cc322165
tovi-cn <1018496570@qq.com>
2017-11-12 14:59:12 +0800

    #

--- src/api/api.js
+++ src/api/api.js
@@ -1,76 +1,84 @@
 import axios from 'axios';
 import qs from 'qs';
-import { Message } from 'element-ui';
+import {
+  Message
+} from 'element-ui';
 
 export const base = 'https://lottery.wns.52jbp.com';
 
 // export const base = 'https://lottery.d1.t1.wxuny.com';
 
 var instance = axios.create({
-    headers: {'content-type': 'application/x-www-form-urlencoded'}
+  headers: {
+    Authorization: localStorage.getItem('token'),
+    'content-type': 'application/x-www-form-urlencoded'
+  }
 });
 
 
 
 // request拦截器
-axios.interceptors.request.use(config => {
-    // Do something before request is sent
-    // config.headers.Authorization = localStorage.getItem('token');
-  
-    // config.headers['Content-Type'] = 'application/x-www-form-urlencoded';
-    config.headers = {
-      Authorization: localStorage.getItem('token'),
-      'Content-Type': 'application/x-www-form-urlencoded'
+instance.interceptors.request.use(config => {
+  // Do something before request is sent
+  // config.headers.Authorization = localStorage.getItem('token');
+
+  // config.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+  config.headers = {
+    Authorization: localStorage.getItem('token'),
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+
+  return config;
+}, error => {
+  // Do something with request error
+  Promise.reject(error);
+})
+
+
+instance.interceptors.response.use(response => {
+  const data = response.data;
+  const errorMsg = data.message;
+  if (data.exception) {
+    response.status = 500;
+    switch (data.exception) {
+      case 200:
+        // return response;
+        break
+      case 401:
+        break;
+      case 404:
+        break;
+      case 500:
+        break;
     }
-  
-    return config;
-  }, error => {
-    // Do something with request error
-    Promise.reject(error);
-  })
-  
-  
-  axios.interceptors.response.use(response => {
-    const data = response.data;
-    const errorMsg = data.message;
-    if (data.exception) {
-      response.status = 500;
-      switch (data.exception) {
-        case 200:
-          // return response;
-          break
-        case 401:
-          break;
-        case 404:
-          break;
-        case 500:
-          break;
-      }
-      Message({
-        message: errorMsg,
-        type: 'error',
-        duration: 5 * 1000
-      });
-      // debugger
-      return Promise.reject(new Error(''));
-    } else {
-      return response;
-    }
-  }, () => {
     Message({
-      message: '网络错误',
+      message: errorMsg,
       type: 'error',
       duration: 5 * 1000
     });
-    // 对返回的错误信息进行处理
+    console.log(data)
     // debugger
     return Promise.reject(new Error(''));
+  } else {
+    return response;
+  }
+}, () => {
+  Message({
+    message: '网络错误',
+    type: 'error',
+    duration: 5 * 1000
   });
+  // 对返回的错误信息进行处理
+  // debugger
+  return Promise.reject(new Error(''));
+});
 
 
 
 //登录
-export const requestLogin = params => { return instance .post(`${base}/auth`, qs.stringify(params)).then(res => res.data); };
+export const requestLogin = params => {
+  return instance.post(`${base}/auth`, qs.stringify(params)).then(res => res.data);
+};
 
 /**
  * 提现请求
@@ -79,4 +87,4 @@ export const requestLogin = params => { return instance .post(`${base}/auth`, qs
 // https://api.cp.52jbp.com/Manager1Open/withdrawList.html
 // form data data:{"user_name":"fff","interval":["2017-07-04","2017-07-04"],"limit":"0,5","hasPass":0,"code":"-1"}
 
-export const drawRequest = params => { return instance .post(`${base}/withdraw`, qs.stringify(params)).then(res => res.data); };
+export const drawRequest = params => instance.get(`${base}/withdraw?${qs.stringify(params)}`).then(res => res.data)
--- src/views/business-flow/withdraw-request.vue
+++ src/views/business-flow/withdraw-request.vue
@@ -2,66 +2,101 @@
 	<section>
 		<!--工具条-->
 		<el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
-			<el-form :inline="true" :model="filters">
+			<el-form :inline="true" :model="search" ref="search">
 				<el-form-item label="用户名">
-					<el-input v-model="filters.name" placeholder="用户名"></el-input>
+					<el-input v-model="search.user_name" placeholder="用户名"></el-input>
 				</el-form-item>
                 <el-form-item label="日期范围">
                     <el-date-picker
-                        v-model="filters.LastLoginTime"
+                        v-model="search.time"
                         type="daterange"
                         align="right"
                         placeholder="选择日期范围"
-                        :picker-options="pickerOptions2">
+                        @change="dateChange">
                     </el-date-picker>
                 </el-form-item>
                 <el-form-item label="状态">
-                    <el-select v-model="filters.status" placeholder="显示全部">
-                    <el-option label="显示全部" value="whole"></el-option>
-                    <el-option label="已提交" value="alreadySubmit"></el-option>
-                    <el-option label="已提现" value="alreadyWD"></el-option>
-                    <el-option label="已失败" value="alreadyLose"></el-option>
-                    </el-select>
+                  <el-select v-model="search.code" placeholder="请选择">
+                      <el-option
+                        v-for="item in options"
+                        :key="item.value"
+                        :label="item.label"
+                        :value="item.value" >
+                      </el-option>
+                  </el-select>
                 </el-form-item>
 
 
 
 				<el-form-item>
-					<el-button type="primary" v-on:click="getUsers">查询</el-button>
-				</el-form-item>
-				<el-form-item>
-					<el-button @click="handleAdd">重置</el-button>
+					<el-button type="primary" @click="onSearch('search')">查询</el-button>
+          <el-button @click="onReset('search')">重置</el-button>
 				</el-form-item>
 			</el-form>
 		</el-col>
 
 		<!--列表-->
-		<el-table :data="users" highlight-current-row v-loading="listLoading" element-loading-text="拼命加载中..." @selection-change="selsChange" style="width: 100%;">
-			<el-table-column prop="name" label="用户名" min-width="120" sortable align="center">
+		<el-table :data="data" highlight-current-row element-loading-text="拼命加载中..." style="width: 100%;">
+			<el-table-column prop="user_name" label="用户名" min-width="120" sortable align="center">
 			</el-table-column>
-			<el-table-column prop="sex" label="上级关系" min-width="100" :formatter="formatSex" align="center">
+			<el-table-column label="上级关系" min-width="200" align="center">
+        <template scope="scope">
+          <el-tooltip class="item" effect="dark" v-bind:content="scope.row.referrers" placement="right">
+            <section>
+            {{scope.row.referrers}}
+            </section>
+          </el-tooltip>
+        </template>
 			</el-table-column>
-			<el-table-column prop="age" label="提现金额" min-width="100" sortable align="center">
+			<el-table-column prop="money" label="提现金额" min-width="100" sortable align="center">
 			</el-table-column>
-			<el-table-column prop="birth" label="银行类型" min-width="120" align="center">
+			<el-table-column prop="bank_type" label="银行类型" min-width="120" align="center">
 			</el-table-column>
-			<el-table-column prop="addr" label="开户姓名" min-width="180" sortable align="center">
+			<el-table-column prop="bank_open_name" label="开户姓名" min-width="180" sortable align="center">
 			</el-table-column>
-            <el-table-column prop="addr" label="开户账号" min-width="180" align="center">
+      <el-table-column label="开户账号" min-width="180" align="center">
+        <template scope="scope">
+          <el-tooltip class="item" effect="dark" v-bind:content="scope.row.bank_account" placement="right">
+            <section>
+            {{scope.row.bank_account}}
+            </section>
+          </el-tooltip>
+        </template>
 			</el-table-column>
-            <el-table-column prop="addr" label="申请时间" min-width="180" sortable align="center">
+      <el-table-column label="申请时间" min-width="180" sortable align="center">
+        <template scope="scope">
+        <el-tooltip class="item" effect="dark" v-bind:content="scope.row.create_time" placement="left">
+          <section>
+          {{scope.row.create_time}}
+          </section>
+        </el-tooltip>
+      </template>
 			</el-table-column>
 			<el-table-column label="操作" width="150" align="center">
 				<template scope="scope">
-					<el-button type="success" size="small" @click="handleAdopt()">通过</el-button>
-					<el-button type="danger" size="small" @click="handleReject()">驳回</el-button>
-				</template>
+          <section v-if="scope.row.code === 1" class="pass">
+            已通过
+          </section>
+          <section v-else-if="scope.row.code === 2" class="reject">
+            已驳回
+          </section>
+          <section v-else>
+            <el-button
+            size="small"
+            type="success"
+            @click="handle(scope.$index, scope.row.id, 1)">通过</el-button>
+          <el-button
+            size="small"
+            type="danger"
+            @click="handle(scope.$index, scope.row.id, 2)">驳回</el-button>
+          </section>
+        </template>
 			</el-table-column>
 		</el-table>
 
 		<!--工具条-->
 		<el-col :span="24" class="toolbar">
-			<el-pagination layout="total,prev, pager, next" @current-change="handleCurrentChange" :page-size="15" :total="total" style="float:right;">
+			<el-pagination layout="total,prev, pager, next" :page-size="search.per_page" :current-page="search.current_page" :total="total" @current-change	="pageChange" style="float:right;">
 			</el-pagination>
 		</el-col>
 
@@ -69,191 +104,106 @@
 </template>
 
 <script>
-	import util from '../../common/js/util'
-	//import NProgress from 'nprogress'
-	import { drawRequest } from '../../api/api';
+import util from "../../common/js/util";
+//import NProgress from 'nprogress'
+import { drawRequest } from "../../api/api";
 
-	export default {
-		data() {
-			return {
-                search: {
-                per_page: 10,
-                current_page: 1,
-                user_name: '',
-                interval: '',
-                code: '',
-                time: []
-                },
-				filters: {
-					name: '',
-                    status:'whole'
-				},
-				users: [],
-				total: 0,
-				page: 1,
-				listLoading: false,
-				sels: [],//列表选中列
+export default {
+  data() {
+    return {
+      total: 0,
+      data: [],
+      search: {
+        per_page: 15,
+        current_page: 1,
+        user_name: "",
+        interval: "",
+        code: "",
+        time: []
+      },
+      options: [
+        {
+          value: "",
+          label: "显示全部"
+        },
+        {
+          value: "-1",
+          label: "已提交"
+        },
+        {
+          value: "1",
+          label: "已提现"
+        },
+        {
+          value: "2",
+          label: "已失败"
+        }
+      ]
+    };
+  },
+  methods: {
+    onReset(formName) {
+      this.$refs[formName].resetFields();
+      this.search.interval = "";
+      this.onSearch();
+    },
+    pageChange(pageNo) {
+      this.search.current_page = pageNo;
+      this.onSubmit();
+    },
+    onSearch() {
+      this.search.current_page = 1;
+      this.onSubmit();
+    },
+    dateChange(event) {
+      if (!event) {
+        this.search.interval = "";
+        return;
+      }
+      const intervalArr = event.split(" - ");
+      this.search.interval =
+        intervalArr[0].trim() + "," + intervalArr[1].trim();
+    },
+    onSubmit() {
+      drawRequest(this.search).then(res => {
+        this.data = res.data;
+        this.total = res.total;
+      });
+    },
+    handle(i, id, code) {
+      this.$confirm(code === 1 ? "确定通过?" : "确定驳回?", "警告", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          handleDrawRequest({ id: id, code: code }).then(res => {
+            this.$message({
+              showClose: true,
+              message: "审核成功",
+              type: "success"
+            });
 
-				editFormVisible: false,//编辑界面是否显示
-				editLoading: false,
-				editFormRules: {
-					name: [
-						{ required: true, message: '请输入姓名', trigger: 'blur' }
-					]
-				},
-				//编辑界面数据
-				editForm: {
-					id: 0,
-					name: '',
-					sex: -1,
-					age: 0,
-					birth: '',
-					addr: ''
-				},
-
-				pickerOptions2:{
-
-                }
-
-			}
-		},
-		methods: {
-			//性别显示转换
-			formatSex: function (row, column) {
-				return row.sex == 1 ? '男' : row.sex == 0 ? '女' : '未知';
-			},
-			handleCurrentChange(val) {
-				this.page = val;
-				this.getUsers();
-			},
-			//获取用户列表
-			getUsers() {
-                drawRequest(this.search).then(res => {
-                    console.log(res);
-                });
-			},
-			//驳回
-			handleReject: function (index, row) {
-				this.$prompt('请输入驳回原因', '提示', {
-                    confirmButtonText: '确定',
-                    cancelButtonText: '取消',
-                }).then(({ value }) => {
-                    this.$message({
-                        type: 'success',
-                        message: '以成功驳回请求'
-                    });
-                }).catch(() => {
-                    this.$message({
-                        type: 'info',
-                        message: '取消驳回操作'
-                    });       
-                });
-			},
-			//通过
-			handleAdopt: function (index, row) {
-				this.$confirm('确定通过?', '警告', {
-                    confirmButtonText: '确定',
-                    cancelButtonText: '取消',
-                    type: 'warning'
-                }).then(() => {
-                    this.$message({
-                        type: 'success',
-                        message: '请求以通过!'
-                    });
-                }).catch(() => {
-                    this.$message({
-                        type: 'info',
-                        message: '取消操作'
-                    });          
-                });
-			},
-			//显示新增界面
-			handleAdd: function () {
-                
-			},
-			//编辑
-			editSubmit: function () {
-				this.$refs.editForm.validate((valid) => {
-					if (valid) {
-						this.$confirm('确认提交吗？', '提示', {}).then(() => {
-							this.editLoading = true;
-							//NProgress.start();
-							let para = Object.assign({}, this.editForm);
-							para.birth = (!para.birth || para.birth == '') ? '' : util.formatDate.format(new Date(para.birth), 'yyyy-MM-dd');
-							editUser(para).then((res) => {
-								this.editLoading = false;
-								//NProgress.done();
-								this.$message({
-									message: '提交成功',
-									type: 'success'
-								});
-								this.$refs['editForm'].resetFields();
-								this.editFormVisible = false;
-								this.getUsers();
-							});
-						});
-					}
-				});
-			},
-			//新增
-			addSubmit: function () {
-				this.$refs.addForm.validate((valid) => {
-					if (valid) {
-						this.$confirm('确认提交吗？', '提示', {}).then(() => {
-							this.addLoading = true;
-							//NProgress.start();
-							let para = Object.assign({}, this.addForm);
-							para.birth = (!para.birth || para.birth == '') ? '' : util.formatDate.format(new Date(para.birth), 'yyyy-MM-dd');
-							addUser(para).then((res) => {
-								this.addLoading = false;
-								//NProgress.done();
-								this.$message({
-									message: '提交成功',
-									type: 'success'
-								});
-								this.$refs['addForm'].resetFields();
-								this.addFormVisible = false;
-								this.getUsers();
-							});
-						});
-					}
-				});
-			},
-			selsChange: function (sels) {
-				this.sels = sels;
-			},
-			//批量删除
-			batchRemove: function () {
-				var ids = this.sels.map(item => item.id).toString();
-				this.$confirm('确认删除选中记录吗？', '提示', {
-					type: 'warning'
-				}).then(() => {
-					this.listLoading = true;
-					//NProgress.start();
-					let para = { ids: ids };
-					batchRemoveUser(para).then((res) => {
-						this.listLoading = false;
-						//NProgress.done();
-						this.$message({
-							message: '删除成功',
-							type: 'success'
-						});
-						this.getUsers();
-					});
-				}).catch(() => {
-
-				});
-			}
-		},
-		mounted() {
-			this.getUsers();
-		}
-	}
-
+            this.$set(this.data[i], "code", code);
+          });
+        })
+        .catch(() => {});
+    }
+  },
+  mounted() {
+    this.onSubmit();
+  }
+};
 </script>
 
 <style scoped>
-    .toolbar{
-        padding: 0px;
-    }
+.pass {
+    color: green;
+  }
+  .reject {
+    color: red;
+  }
+.toolbar {
+  padding: 0px;
+}
 </style>
